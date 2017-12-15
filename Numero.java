369e21ca8ad1f34fc9ea13f91cbcdebf7291af64
Rosa Santos <micoledejaen@gmail.com>
2017-12-15 07:40:58 +0100

    Primer commit

---
+++ Numero.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+
+/**
+ 
+ */
+
+
+public class Numero {
+	
+	public static void main (String[] args) {
+	Scanner sc=new Scanner(System.in);
+	System.out.println("Introduzca el valor del primer número, mayor que cero y menor que mil");
+	int n1=sc.nextInt();
+	System.out.println("Introduzca el valor del segundo número, mayor que cero y menor que mil");	
+	int n2=sc.nextInt();	
+	sc.close();
+	
+	}
+}
+
